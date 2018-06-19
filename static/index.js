c4f59190b4a9033d8a69597e4ee3c95a6e4cf8b0
Casey Rodarmor <casey@rodarmor.com>
2018-06-19 15:36:10 -0400

    Add binary runner

---
+++ static/index.js
@@ -0,0 +1,25 @@
+var importObject = {
+  imports: {
+    print: arg => console.log(arg),
+  },
+};
+
+async function load() {
+  console.log('fetching main.wasm...');
+  let response = await fetch('main.wasm');
+
+  console.log('getting wasm bytes...');
+  let bytes = await response.arrayBuffer();
+
+  console.log('compiling webassembly module...');
+  let module = await WebAssembly.compile(bytes);
+
+  console.log('constructing webassembly instance...');
+  let instance = new WebAssembly.Instance(module, importObject);
+
+  console.log('running webassembly main...');
+  instance.exports.main();
+}
+
+console.log('calling load...');
+load();
