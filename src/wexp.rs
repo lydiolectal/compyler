b04eec080a12fbc692c3e4876a3fb441ba02e4b4
lydiolectal <lydiding@gmail.com>
2018-06-20 14:01:15 -0400

    Add file for wexp and Display for wexp

---
+++ src/wexp.rs
@@ -0,0 +1,48 @@
+use std::fmt::{self, Display, Formatter};
+
+pub enum Wexp {
+  List(Vec<Wexp>),
+  Atom(String),
+}
+
+impl Display for Wexp {
+  fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+    use self::Wexp::*;
+    match self {
+      List(contents) => {
+        write!(f, "(")?;
+        for (i, wexp) in contents.iter().enumerate() {
+          write!(f, "{}", wexp)?;
+          if i < contents.len() - 1 {
+            write!(f, " ")?;
+          }
+        }
+        write!(f, ")")
+      },
+      Atom(value) => write!(f, "{}", value),
+    }
+  }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn display_atom() {
+        let w = Wexp::Atom("hallo".to_owned());
+        assert_eq!(w.to_string(), "hallo");
+    }
+
+    #[test]
+    fn display_empty_vec() {
+        let w = Wexp::List(vec![]);
+        assert_eq!(w.to_string(), "()");
+    }
+
+    #[test]
+    fn display_list() {
+        let w = Wexp::List(vec![Wexp::Atom("hmm".to_owned()), Wexp::List(vec![Wexp::Atom("??!".to_owned())])]);
+        assert_eq!(w.to_string(), "(hmm (??!))");
+    }
+}
