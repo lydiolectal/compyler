dd833355032410853b77c3f8f538dc85cc953395
lydiolectal <lydiding@gmail.com>
2018-07-13 20:14:09 -0400

    Generated code for fibonacci!

--- src/codegen.rs
+++ src/codegen.rs
@@ -349,4 +349,29 @@ mod test {
             call $f \
             call $print))",
     }
+
+    codegen_test! {
+        name: fib,
+        text: "def fib(n):\n  if n < 2:\n    return n\n  else:\n    return fib(n - 2) + fib(n - 1)",
+        wat: "(module \
+            (func $print (import \"host\" \"print\") (param i32)) \
+            (func $fib (param $n i32) (result i32) \
+            get_local $n \
+            i32.const 2 \
+            i32.lt_s \
+            if (result i32) \
+            get_local $n \
+            else \
+            get_local $n \
+            i32.const 2 \
+            i32.sub \
+            call $fib \
+            get_local $n \
+            i32.const 1 \
+            i32.sub \
+            call $fib \
+            i32.add \
+            end) \
+            (func (export \"main\")))",
+    }
 }
